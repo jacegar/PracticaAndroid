e1896d69b639cecc298fdf3a5794ad896465e61c
Juan Acevedo <160587907+jacegar@users.noreply.github.com>
2026-01-04 18:10:06 +0100

    Ejercicios predefinidos

--- app/src/main/java/com/example/practicaandroid/data/AppDatabase.java
+++ app/src/main/java/com/example/practicaandroid/data/AppDatabase.java
@@ -126,21 +126,184 @@ public abstract class AppDatabase extends RoomDatabase {
                 List<Ejercicio> ejercicioList = new ArrayList<>();
 
                 ejercicioList.add(new Ejercicio(
-                        "res:ejercicio_res_name_flexiones",
-                        "res:ejercicio_res_desc_flexiones",
-                        "FUERZA"
+                        "res:ejercicio_res_name_press_banca",
+                        "res:ejercicio_res_desc_press_banca",
+                        "strength_type"
                 ));
 
                 ejercicioList.add(new Ejercicio(
-                        "res:ejercicio_res_name_sentadillas",
-                        "res:ejercicio_res_desc_sentadillas",
+                        "res:ejercicio_res_name_press_inclinado_mancuernas",
+                        "res:ejercicio_res_desc_press_inclinado_mancuernas",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_aperturas_mancuernas",
+                        "res:ejercicio_res_desc_aperturas_mancuernas",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_jalon_pecho",
+                        "res:ejercicio_res_desc_jalon_pecho",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_remo_barra",
+                        "res:ejercicio_res_desc_remo_barra",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_remo_polea_baja",
+                        "res:ejercicio_res_desc_remo_polea_baja",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_peso_muerto",
+                        "res:ejercicio_res_desc_peso_muerto",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_sentadilla_barra",
+                        "res:ejercicio_res_desc_sentadilla_barra",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_prensa_piernas",
+                        "res:ejercicio_res_desc_prensa_piernas",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_extension_cuadriceps",
+                        "res:ejercicio_res_desc_extension_cuadriceps",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_curl_femoral",
+                        "res:ejercicio_res_desc_curl_femoral",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_elevaciones_gemelos_maquina",
+                        "res:ejercicio_res_desc_elevaciones_gemelos_maquina",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_press_militar",
+                        "res:ejercicio_res_desc_press_militar",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_elevaciones_laterales",
+                        "res:ejercicio_res_desc_elevaciones_laterales",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_curl_biceps_barra",
+                        "res:ejercicio_res_desc_curl_biceps_barra",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_curl_biceps_mancuernas",
+                        "res:ejercicio_res_desc_curl_biceps_mancuernas",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_extension_triceps_polea",
+                        "res:ejercicio_res_desc_extension_triceps_polea",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_press_frances",
+                        "res:ejercicio_res_desc_press_frances",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_abdominales_maquina",
+                        "res:ejercicio_res_desc_abdominales_maquina",
+                        "strength_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_cinta_correr",
+                        "res:ejercicio_res_desc_cinta_correr",
+                        "cardio_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_bicicleta_estatica",
+                        "res:ejercicio_res_desc_bicicleta_estatica",
+                        "cardio_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_eliptica",
+                        "res:ejercicio_res_desc_eliptica",
+                        "cardio_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_remo_ergometro",
+                        "res:ejercicio_res_desc_remo_ergometro",
+                        "cardio_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_escalera_gimnasio",
+                        "res:ejercicio_res_desc_escalera_gimnasio",
+                        "cardio_type"
+                ));
+
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_estiramiento_isquiotibiales",
+                        "res:ejercicio_res_desc_estiramiento_isquiotibiales",
                         "flexibility_type"
                 ));
 
                 ejercicioList.add(new Ejercicio(
-                        "res:ejercicio_res_name_plancha",
-                        "res:ejercicio_res_desc_plancha",
-                        "cardio_type"
+                        "res:ejercicio_res_name_estiramiento_cuadriceps",
+                        "res:ejercicio_res_desc_estiramiento_cuadriceps",
+                        "flexibility_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_estiramiento_gluteos",
+                        "res:ejercicio_res_desc_estiramiento_gluteos",
+                        "flexibility_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_estiramiento_pectoral",
+                        "res:ejercicio_res_desc_estiramiento_pectoral",
+                        "flexibility_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_estiramiento_dorsal",
+                        "res:ejercicio_res_desc_estiramiento_dorsal",
+                        "flexibility_type"
+                ));
+
+                ejercicioList.add(new Ejercicio(
+                        "res:ejercicio_res_name_movilidad_hombros",
+                        "res:ejercicio_res_desc_movilidad_hombros",
+                        "flexibility_type"
                 ));
 
                 dao.insertAll(ejercicioList);
